ee6c5bd386a2d9ac629b1ea33aa565ce02cb612e
qbcir <cmpxchg32@gmail.com>
2022-04-25 09:40:37 +0200

    initial commit

---
+++ AlienCell.Server/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AlienCell.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+            services.AddMagicOnion();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapMagicOnionService();
+            });
+        }
+    }
+}
+
---
+++ AlienCell.Shared/IMyFirstService.cs
@@ -0,0 +1,14 @@
+using System;
+using MagicOnion;
+
+namespace AlienCell.Shared
+{
+    // Defines .NET interface as a Server/Client IDL.
+    // The interface is shared between server and client.
+    public interface IMyFirstService : IService<IMyFirstService>
+    {
+        // The return type must be `UnaryResult<T>`.
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
+
